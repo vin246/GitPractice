dff6335e18d7434338ac2ca7da6347c63c723277
User <User@DESKTOP-VPS5K8P>
2021-12-15 18:06:17 +0530

    commit successfully

---
+++ src/test/java/com/crm/Practise/Sample.java
@@ -0,0 +1,11 @@
+package com.crm.Practise;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	@Test
+	public void sample() {
+		System.out.println("user1 created sucessfully");
+	}
+
+}
