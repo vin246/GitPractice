5876ac892277c1be7f555e99a0a1ad70252493e8
dasar <dasar@NAVYUGA>
2021-12-15 16:36:48 +0530

    First Commit

---
+++ src/test/java/com/crm/Practise/Demo4Test.java
@@ -0,0 +1,11 @@
+package com.crm.Practise;
+
+import org.testng.annotations.Test;
+
+public class Demo4Test{
+@Test
+public void demo() {
+	System.out.println("Welcome To Maven");
+	System.out.println("User1 -----> Created");
+}
+}
